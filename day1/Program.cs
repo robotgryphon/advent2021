6fb65ac11ab7b5a7bb20c555481d46952920dfe3
Ted Senft <code@robotgryphon.com>
2021-12-01 18:04:10 -0500

    Day 1 Part 1

---
+++ day1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+var lines = await System.IO.File.ReadAllLinesAsync("input.txt");
+int lastValue = int.Parse(lines[0]);
+int more = 0;
+for(int l = 1; l < lines.Length; l++) {
+    if(int.Parse(lines[l]) > lastValue) more++;
+    lastValue = int.Parse(lines[l]);
+}
+
+Console.WriteLine(more);
