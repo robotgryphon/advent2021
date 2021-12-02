559bf98b4e86d012d92e0193d9b7812b02153cbb
Ted Senft <code@robotgryphon.com>
2021-12-02 03:46:44 -0500

    Part 1 - Day 2

---
+++ day2/Program.cs
@@ -0,0 +1,14 @@
+﻿var lines = await System.IO.File.ReadAllLinesAsync("input.txt");
+int forw = 0;
+int depth = 0;
+
+foreach(string cmd in lines) {
+    int amt = int.Parse(cmd.Split(" ")[1]);
+    switch(cmd.Split(" ")[0].ToLower()){
+        case "forward": forw += amt; break;
+        case "down": depth += amt; break;
+        case "up": depth -= amt; break;
+    }
+}
+
+Console.WriteLine($"Forward ({forw}) * Depth ({depth}) = {forw * depth}");
