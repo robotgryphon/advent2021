0a979251d981502ce1bb1283e7f8cb9b7f57b871
Ted Senft <code@robotgryphon.com>
2021-12-07 09:13:39 -0500

    Day 7-2

--- Day 7/Program.cs
+++ Day 7/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Threading.Tasks.Dataflow;
+
+var timer = Stopwatch.StartNew();
 
 string input;
 
@@ -14,13 +17,18 @@ int[] positions = File.ReadAllLines(input).First().Split(",").Select(x => int.Pa
 ConcurrentBag<Tuple<int, int>> results = new ConcurrentBag<Tuple<int, int>>();
 
 var posToFuelConverter = new TransformBlock<int, Tuple<int, int[]>>(ConvertToFuel);
-var fuelToTotal = new TransformBlock<Tuple<int, int[]>, Tuple<int, int>>((fuel) => Tuple.Create(fuel.Item1, fuel.Item2.Sum()));
+var fuelToTotal = new TransformBlock<Tuple<int, int[]>, Tuple<int, int>>((fuel) =>
+{
+    Console.WriteLine($"Movement to {fuel.Item1} = {string.Join(',', fuel.Item2)}");
+    return Tuple.Create(fuel.Item1, fuel.Item2.Sum());
+});
+
 var collect = new ActionBlock<Tuple<int, int>>(results.Add);
 
 posToFuelConverter.LinkTo(fuelToTotal, new DataflowLinkOptions { PropagateCompletion = true });
 fuelToTotal.LinkTo(collect, new DataflowLinkOptions { PropagateCompletion = true });
 
-foreach (int target in positions.Distinct())
+foreach (int target in Enumerable.Range(0, positions.Max()))
     posToFuelConverter.Post(target);
 
 posToFuelConverter.Complete();
@@ -43,13 +51,21 @@ foreach (var r in res)
     Console.WriteLine(" fuel.");
 }
 
+timer.Stop();
+
+Console.WriteLine($"Done in {timer.ElapsedMilliseconds}ms. ({timer.Elapsed.TotalSeconds}s)");
+
 Tuple<int, int[]> ConvertToFuel(int target)
 {
     // Console.WriteLine("Converting to fuel values: " + target);
 
     int[] fuel = new int[positions.Length];
     for (int crab = 0; crab < positions.Length; crab++)
-        fuel[crab] = Math.Abs(positions[crab] - target);
+    {
+        int numMoves = Math.Abs(positions[crab] - target);
+        if (numMoves == 0) fuel[crab] = 0;
+        else fuel[crab] = Enumerable.Range(0, numMoves + 1).Sum();
+    }
 
     return Tuple.Create(target, fuel);
 }
