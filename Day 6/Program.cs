2c352537cad371e550df1d484dfeb63fc9fbab1e
Ted Senft <code@robotgryphon.com>
2021-12-07 08:17:50 -0500

    Day 6 Solution

---
+++ Day 6/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using RobotGryphon.AdventOfCode2021.Day6;
+
+string[] fishIn = await File.ReadAllLinesAsync("lanternfish.txt");
+int[] fishInitial = fishIn[0].Split(",").Select(int.Parse).ToArray();
+
+School school = new School(fishInitial);
+school.Simulate(256);
+Console.WriteLine("Amount of fish after 256 days: " + school.Fish);
---
+++ Day 6/School.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotGryphon.AdventOfCode2021.Day6
+{
+    public class School
+    {
+        public long Fish { get; set; }
+        public int[] Lifespans;
+
+        public School(int[] lifespans)
+        {
+            this.Lifespans = lifespans;
+            Fish = lifespans.Length;
+        }
+
+        public School Simulate(int days)
+        {
+            List<long> fishCounts = Enumerable.Range(0, 8 + 1)
+                .Select(days => (long) Lifespans.Count(f => f == days))
+                .ToList();
+
+            foreach (int day in Enumerable.Range(0, days))
+            {
+                // the number of fish with 0 days left is now the first element
+                long spawning = fishCounts.First();
+
+                // shift all fish down a day (rolling the 0-day fish off the front of the list)
+                fishCounts = fishCounts.Skip(1).ToList();
+
+                // all the fish that rolled off the front get added again with 6 days left
+                fishCounts[6] += spawning;
+
+                // and contribute that many new fish with 8 days left
+                fishCounts.Add(spawning);
+            }
+
+            this.Fish = fishCounts.Sum();
+            return this;
+        }
+    }
+}
