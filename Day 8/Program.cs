ba11d3e3e33994c95eea7336740de107cbe9c357
Ted Senft <code@robotgryphon.com>
2021-12-09 18:26:36 -0500

    WIP Day 8 - 2

--- Day 8/Program.cs
+++ Day 8/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using Day_8;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Threading.Tasks.Dataflow;
 
@@ -14,59 +15,146 @@ input = "digits.txt";
 
 string[] digits = await File.ReadAllLinesAsync(input);
 
-var DigitSegments = new Dictionary<int, char[]>
-{
-    { 0, new char[] { 'a', 'b', 'c', 'e', 'f', 'g' } },
-    { 1, new char[] { 'c', 'f' } },
-    { 2, new char[] { 'a', 'c', 'd', 'e', 'g' } },
-    { 3, new char[] { 'a', 'c', 'd', 'f', 'g' } },
-    { 4, new char[] { 'b', 'c', 'd', 'f' } },
-    { 5, new char[] { 'a', 'b', 'd', 'f', 'g' } },
-    { 6, new char[] { 'a', 'b', 'd', 'e', 'f', 'g' } },
-    { 7, new char[] { 'a', 'c', 'f' } },
-    { 8, new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
-    { 9, new char[] { 'a', 'b', 'c', 'd', 'f', 'g' } },
-};
+//int top = Console.CursorTop;
+//int start = Console.CursorLeft;
+//foreach (int digit in Enumerable.Range(8, 1))
+//{
+//    SegmentedDisplay.Display(digit, new char[] { 'd', 'e', 'a', 'f', 'g', 'b', 'c' });
+//    Console.CursorLeft = start + ((digit + 1) * 7);
+//    Console.CursorTop = top;
+//}
 
-Dictionary<int, int[]> determined = new Dictionary<int, int[]>();
+//Console.CursorTop = start + 9;
+//return 0;
+
+
 int currLine = 0;
-foreach (string entry in digits)
+foreach (string entry in digits.Take(1))
 {
+    Dictionary<string, List<int>> guesses = new();
+    Dictionary<char, List<char>> remapGuesses = new();
+    Dictionary<char, char> remapped = new();
+
+    foreach (char seg in SegmentedDisplay.AllSegments)
+        remapGuesses.Add(seg, new List<char>(SegmentedDisplay.AllSegments));
+
     char[][] segments = entry.Split('|')[0].Split(' ').Select(x => x.ToCharArray()).ToArray();
     string[] outputs = entry.Split('|')[1].Split(' ').Select(x => string.Join("", x.ToCharArray().OrderBy(x => x))).ToArray();
 
-    Dictionary<int, List<int>> guesses = new Dictionary<int, List<int>>();
-    for (int gind = 0; gind < segments.Length; gind++)
-    {
-        char[] group = segments[gind];
-        guesses.Add(gind, DigitSegments.Where(seg => seg.Value.Length == group.Length).Select(x => x.Key).ToList());
-    }
+    GetGuessesFromLine(guesses, segments, outputs);
 
-    Dictionary<int, string> known = new Dictionary<int, string>();
-    foreach (var guessGroup in guesses)
-    {
-        if (guessGroup.Value.Count == 1)
-        {
-            known.Add(guessGroup.Value[0], string.Join("", segments[guessGroup.Key].OrderBy(x => x)));
-        }
-    }
+    Dictionary<int, string> known = guesses.Where(x => x.Value.Count == 1).ToDictionary(x => x.Value[0], x => x.Key);
 
-    Dictionary<int, int> determinedEntry = new();
+    foreach (string kno in known.Values)
+        guesses.Remove(kno);
+
     foreach (var k in known.Keys)
     {
-        int numAppearances = outputs.Count(o => o == known[k]);
-        determinedEntry.Add(k, numAppearances);
+        var removeableGuesses = guesses.Where(g => g.Value.Contains(k)).Select(k => k.Key);
+        foreach (string gind in removeableGuesses)
+            guesses[gind].Remove(k);
     }
 
-    var found = string.Join(',', determinedEntry.Where(x => x.Value > 0));
-    Console.WriteLine(entry + " = " + found + " = " + determinedEntry.Values.Sum());
-    List<int> entries = new List<int>();
-    foreach (var d in determinedEntry.Keys)
-        for (int i = 0; i < determinedEntry[d]; i++) entries.Add(d);
+    if(known.Count < 4)
+        Console.WriteLine("Known #:" + known.Count);
 
-    determined.Add(currLine, entries.ToArray());
+    // calculate 2,3,5
+    char[][] guesses235 = guesses.Where(x => x.Key.Length == 5).Select(x => x.Key.ToCharArray()).ToArray();
+    char[][] guesses069 = guesses.Where(x => x.Key.Length == 6).Select(x => x.Key.ToCharArray()).ToArray();
+
+    // if we don't have 1 we can determine by diffing 4 and 7
+    if (!known.ContainsKey(1) && (known.ContainsKey(4) && known.ContainsKey(7)))
+    {
+        known[1] = known[7].Replace(remapped['a'].ToString(), "");
+    }
+
+    // remove all guesses from known right segments, provided by 1
+    if (known.ContainsKey(1) && known.ContainsKey(4))
+    {
+        remapGuesses['b'].RemoveAll(ch => known[1].Contains(ch));
+        remapGuesses['d'].RemoveAll(ch => known[1].Contains(ch));
+    }
+
+    // top segment
+    if (known.ContainsKey(1) && known.ContainsKey(7))
+    {
+        remapped['a'] = known[7].Except(known[1]).First();
+        remapGuesses.Remove('a');
+    }
+
+    // determine 9
+    if (known.ContainsKey(4))
+    {
+        var known9 = guesses.Where(x => x.Key.Length == 6).First(x => known[4].All(c => x.Key.Contains(c)));
+        known[9] = known9.Key;
+
+        // remove all of 4's segments from 9's segment guesses
+        foreach (char s9 in SegmentedDisplay.DigitSegments[9])
+            if (remapGuesses.ContainsKey(s9))
+                remapGuesses[s9].RemoveAll(guess => known[4].Contains(guess));
+    }
+
+    if (known.ContainsKey(4) && known.ContainsKey(7))
+    {
+        remapped['a'] = known[7].Except(known[4]).First();
+        remapGuesses.Remove('a');
+
+        var digitsUsingA = remapGuesses.Where(g => g.Value.Contains('a')).Select(x => x.Key);
+        foreach (var b in digitsUsingA)
+            remapGuesses[b].Remove(remapped['a']);
+    }
+
+
+
+    //foreach (int digit in SegmentedDisplay.DigitSegments.Keys.Except(known.Keys))
+    //{
+    //    remapGuesses[digit].RemoveAll(c => known.Where(x => x.Key != 8).SelectMany(x => x.Value).Contains(c));
+    //}
+
     currLine++;
+
+    //Console.WriteLine();
+    //WriteDigitLine(known, remapped);
+
+    Console.WriteLine();
+    Console.WriteLine();
+    WriteDigitLine(known, remapped);
 }
 
-var counts = determined.Select(x => x.Value).Sum(x => x.Length);
-Console.WriteLine("All determined: " + counts);
+timer.Stop();
+
+static void GetGuessesFromLine(Dictionary<string, List<int>> guesses, char[][] segments, string[] outputs)
+{
+    for (int gind = 0; gind < segments.Length; gind++)
+    {
+        char[] group = segments[gind].OrderBy(x => x).ToArray();
+        if (group.Length == 0) continue;
+        guesses.Add(String.Join("", group), SegmentedDisplay.DigitSegments.Where(seg => seg.Value.Length == group.Length).Select(x => x.Key).ToList());
+    }
+
+    for (int gind = 0; gind < outputs.Length; gind++)
+    {
+        char[] group = outputs[gind].OrderBy(x => x).ToArray();
+        if (group.Length == 0) continue;
+        string found = String.Join("", group);
+        if (!guesses.ContainsKey(found))
+            guesses.Add(found, SegmentedDisplay.DigitSegments.Where(seg => seg.Value.Length == group.Length).Select(x => x.Key).ToList());
+    }
+}
+
+static void WriteDigitLine(Dictionary<int, string> known, Dictionary<char, char> remapped)
+{
+    int top = Console.CursorTop;
+    int start = Console.CursorLeft;
+    foreach (int digit in Enumerable.Range(0, 10))
+    {
+        if (known.ContainsKey(digit))
+            SegmentedDisplay.Display(digit, SegmentedDisplay.Remap(digit, remapped));
+
+        Console.CursorLeft = start + ((digit + 1) * 7);
+        Console.CursorTop = top;
+    }
+
+    Console.CursorTop = top + 9;
+    Console.CursorLeft = start;
+}
---
+++ Day 8/SegmentedDisplay.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8
+{
+    public class SegmentedDisplay
+    {
+        internal static readonly Dictionary<int, char[]> DigitSegments = new()
+        {
+            { 0, new char[] { 'a', 'b', 'c', 'e', 'f', 'g' } },
+            { 1, new char[] { 'c', 'f' } },
+            { 2, new char[] { 'a', 'c', 'd', 'e', 'g' } },
+            { 3, new char[] { 'a', 'c', 'd', 'f', 'g' } },
+            { 4, new char[] { 'b', 'c', 'd', 'f' } },
+            { 5, new char[] { 'a', 'b', 'd', 'f', 'g' } },
+            { 6, new char[] { 'a', 'b', 'd', 'e', 'f', 'g' } },
+            { 7, new char[] { 'a', 'c', 'f' } },
+            { 8, new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' } },
+            { 9, new char[] { 'a', 'b', 'c', 'd', 'f', 'g' } },
+        };
+
+        public static char[] AllSegments => DigitSegments[8];
+
+        public static char[] Unscramble(char[] input)
+        {
+            switch(input.Length)
+            {
+                case 2: return DigitSegments[1];
+                case 4: return DigitSegments[4];
+                case 3: return DigitSegments[7];
+                case 8: return DigitSegments[8];
+            }
+
+            int[] possible = DigitSegments.Where(ds => ds.Value.Length == input.Length).Select(ds => ds.Key).ToArray();
+            return DigitSegments[possible[0]];
+        }
+
+        public static void Display(int digit, char[]? remap = null)
+        {
+            Dictionary<char, char> segments = new();
+            if (remap != null)
+            {
+                for(int i = 0; i < DigitSegments[digit].Length; i++)
+                    segments.Add(DigitSegments[digit][i], remap[i]);
+            } else
+            {
+                segments = DigitSegments[digit].ToDictionary(d => d);
+            }
+
+            int startX = Console.CursorLeft;
+            int startY = Console.CursorTop;
+
+            var o = Console.Out;
+
+            if (segments.ContainsKey('a'))
+            {
+                WriteHorizontal(o, segments['a']);
+                Console.SetCursorPosition(startX, startY);
+            }
+
+            foreach(var v1 in Enumerable.Range(startY + 1, 3)) {
+                Console.SetCursorPosition(startX, v1);
+                o.Write(segments.ContainsKey('b') ? segments['b'] : ' ');
+                o.Write("   ");
+                o.Write(segments.ContainsKey('c') ? segments['c'] : ' ');
+                Console.SetCursorPosition(startX, startY);
+            }
+
+            if (segments.ContainsKey('d'))
+            {
+                Console.SetCursorPosition(startX, startY + 4);
+                WriteHorizontal(o, segments['d']);
+                Console.SetCursorPosition(startX, startY);
+            }
+
+            foreach (var v2 in Enumerable.Range(startY + 5, 3))
+            {
+                Console.SetCursorPosition(startX, v2);
+                o.Write(segments.ContainsKey('e') ? segments['e'] : ' ');
+                o.Write("   ");
+                o.Write(segments.ContainsKey('f') ? segments['f'] : ' ');
+                Console.SetCursorPosition(startX, startY);
+            }
+
+            if (segments.ContainsKey('g'))
+            {
+                Console.SetCursorPosition(startX, startY + 8);
+                WriteHorizontal(o, segments['g']);
+                Console.SetCursorPosition(startX, startY);
+            }
+        }
+
+        private static void WriteHorizontal(TextWriter o, char digit)
+        {
+            o.Write(' ');
+            o.Write("".PadLeft(3, digit));
+            o.Write(' ');
+        }
+
+        internal static char[]? Remap(int digit, Dictionary<char, char> remapped)
+        {
+            char[] original = DigitSegments[digit];
+            char[] map = new char[original.Length];
+            for(int op = 0; op < original.Length; op++)
+            {
+                if (remapped.ContainsKey(original[op]))
+                    map[op] = remapped[original[op]];
+                else
+                    map[op] = ' ';
+            }
+
+            return map;
+        }
+    }
+}
